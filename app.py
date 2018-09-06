d52a7ff337d4a53e4a3abb829b0a5a324ea75d53
Vladimir Andreev <vandreevwork94@gmail.com>
2018-09-06 21:39:57 +0300

    Add admin panel

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from flask_marshmallow import Marshmallow
-from flask_restful_swagger import swagger
 from flask_cors import CORS
 from flask_env import MetaFlaskEnv
 
@@ -23,9 +22,5 @@ CORS(myApp)
 myDb = SQLAlchemy(myApp)
 
 myApi = Api(myApp)
-
-# Если дебаг режим, подключаем сваггер доку, потыкаться можно по адресу http://127.0.0.1:5000/api/spec.html#!/spec
-if myApp.debug:
-    myApi = swagger.docs(Api(myApp), description="Description serials API", apiVersion='0.1.0')
 
 myMa = Marshmallow(myApp)
--- index.py
+++ index.py
@@ -1,8 +1,61 @@
+from werkzeug.wrappers import Response
+from werkzeug.utils import redirect
+from werkzeug.exceptions import HTTPException
+
 from app import myApi, myApp, myDb
-from resourses.episodes import EpisodesResource
+from flask_restful import Api
+from models.serials import Serial
+from resourses.episodes import EpisodesResource, Episode
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.contrib.fileadmin import FileAdmin
+from flask_restful_swagger import swagger
+from flask_admin import Admin
+from flask_basicauth import BasicAuth
+import os
+
 
 myApi.add_resource(EpisodesResource, '/episodes')
 
+# - сваггер доку, потыкаться можно по адресу http://127.0.0.1:5000/api/spec.html#!/spec
+# - админку для зависимостей
+myApp.config['BASIC_AUTH_USERNAME'] = os.environ.get('BASIC_AUTH_USERNAME')
+myApp.config['BASIC_AUTH_PASSWORD'] = os.environ.get('BASIC_AUTH_PASSWORD')
+
+myBasicAuth = BasicAuth(myApp)
+
+myApi = swagger.docs(Api(myApp), description="Description serials API", apiVersion='0.1.0')
+
+
+class AuthException(HTTPException):
+    def __init__(self, message):
+        super().__init__(message, Response(
+            "You could not be authenticated. Please refresh the page.", 401,
+            {'WWW-Authenticate': 'Basic realm="Login Required"'}
+        ))
+
+
+class MyModelView(ModelView):
+    def is_accessible(self):
+        if not myBasicAuth.authenticate():
+            raise AuthException('Not authenticated.')
+        else:
+            return True
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(myBasicAuth.challenge())
+
+
+# Чтобы работала сессия базы на запись
+myApp.secret_key = os.environ.get('SECRET_KEY')
+myApp.config['SESSION_TYPE'] = 'filesystem'
+
+myAdmin = Admin(myApp, name='english-db')
+
+myAdmin.add_view(MyModelView(Serial, myDb.session))
+myAdmin.add_view(MyModelView(Episode, myDb.session))
+
+if os.environ.get('STATIC_DIR'):
+    myAdmin.add_view(FileAdmin(os.environ.get('STATIC_DIR'), name='Static Files'))
 
 def start_server():
     myApp.run(host='0.0.0.0')
